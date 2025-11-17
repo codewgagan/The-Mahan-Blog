389b7d8dff19ecbda54488cef0b7091fa853a5ba
Gagan Gowda <122716450+codewgagan@users.noreply.github.com>
2025-11-18 05:21:57 +0530

    Created Home Page

---
+++ src/components/BlogList.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const BlogList = () => {
+  return (
+    <div>
+      <div className=''>
+            
+      </div>
+    </div>
+  )
+}
+
+export default BlogList
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -7,9 +7,34 @@ const Header = () => {
       <div className="text-center mt-20 mb-8">
         <div className="inline-flex items-center justify-center gap-4 px-6 py-1.5 mb-4 border-primary/40 bg-primary/10 rounded-full text-sm text-primary">
           <p>The AI Integrated</p>
+
           <img src={assets.star_icon} alt="star-icon" />
         </div>
+
+        <h1 className="text-3xl sm:text-6xl font-semibold text-gray-700 sm:leading-16">
+          {" "}
+          Welcome to The Mahan <span className="text-primary">
+            Blogging
+          </span>{" "}
+          <br /> Platform{" "}
+        </h1>
+        <div className="">
+          <p className="text-gray-700">
+            The Mahan Blog is your guide to simple, effective, and sustainable
+            fitness. Grow stronger every day physically and mentally.
+          </p>
+        </div>
+        <form className="flex justify-between max-w-lg max-sm:scale-75 mx-auto border border-gray-300 bg-white rounded overflow-hidden">
+          <input type="text" placeholder="search here" id="" className="w-full pl-4 outline-none" />
+          <button className="bg-primary text-white px-8 py-2 m-1.5 rounded hover:scale-105 transition-all cursor-pointer">Search</button>
+        </form>
       </div>
+      <img
+        src={assets.gradientBackground}
+        className="absolute -top-50 -z-1 opacity-50"
+        alt=""
+        srcset=""
+      />
     </div>
   );
 };
