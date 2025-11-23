7333f6dd5f3f1bc04e727f6463c7614eaab81ed3
Gagan Gowda <122716450+codewgagan@users.noreply.github.com>
2025-11-23 23:47:08 +0530

    Created Home Page

--- src/components/BlogCard.jsx
+++ src/components/BlogCard.jsx
@@ -1,11 +1,31 @@
-import React from 'react'
+import React from "react";
+import { useNavigate } from "react-router-dom";
 
-const BlogCard = ({blog}) => {
+const BlogCard = ({ blog }) => {
+  // blog data
+  const { title, category, description, image, _id } = blog;
+  const navigate = useNavigate();
   return (
-    <div>
-      
+    <div
+      onClick={() => navigate(`/blog/${_id}`)}
+      className="w-full rounded-lg overflow-hidden shadow hover:scale-105 hover:shadow-primary/25 duration-300 cursor-pointer"
+    >
+      {/* Card details */}
+      {/*1. image */}
+      <img src={image} alt="" className="aspect-video" />
+      {/* 2. category in span tag */}
+      <span className="ml-5 mt-4 px-3 py-1 inline-block bg-primary/20 rounded-full text-primary text-xs">
+        {category}
+      </span>
+      <div>
+        {/* 3. title */}
+        <h5 className="mb-2 font-medium text-gray-900">{title}</h5>
+        {/* 4. Description with slice */}
+        <p className="mb-3 text-xs text-gray-600 " dangerouslySetInnerHTML={{"__html": description.slice(0, 80)}}></p>
+      </div>
     </div>
-  )
-}
+  );
+//   {description.slice(0, 80)}
+};
 
-export default BlogCard
+export default BlogCard;
---
+++ src/components/NewsLetter.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const NewsLetter = () => {
+  return (
+    <div>
+      <h1>Never Miss this knowledge</h1>
+      <p>Subacaribe</p>
+      <form>
+
+        <input type="text" name="" id="" />
+      </form>
+    </div>
+  )
+}
+
+export default NewsLetter
