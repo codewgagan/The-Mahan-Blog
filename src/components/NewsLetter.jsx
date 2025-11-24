62e178b5963e79a8763a45af6800e4c4e6bc357e
Gagan Gowda <122716450+codewgagan@users.noreply.github.com>
2025-11-24 23:59:44 +0530

    Created Home Page

--- src/components/NewsLetter.jsx
+++ src/components/NewsLetter.jsx
@@ -2,12 +2,13 @@ import React from 'react'
 
 const NewsLetter = () => {
   return (
-    <div>
-      <h1>Never Miss this knowledge</h1>
+    <div className='flex flex-col items-center justify-center text-center space-y-2'>
+      <h1 className='md:text-4xl text-2xl'>Never Miss this knowledge</h1>
       <p>Subacaribe</p>
       <form>
 
         <input type="text" name="" id="" />
+        <button type='submit' className='md:px-12 px-8 h-full text-white bg-primary/80 hover:bg-primary transition-all cursor-pointer rounded-md'>Subscribe</button>
       </form>
     </div>
   )
